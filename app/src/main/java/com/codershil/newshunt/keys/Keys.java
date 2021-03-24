7f05224e090d2e86ae8c1883f8b89c05dbb25553
shil-alone <shilalone007@gmail.com>
2021-03-24 19:24:51 +0530

    added gitignore to packages folder

--- app/src/main/java/com/codershil/newshunt/keys/Keys.java
+++
@@ -1,9 +0,0 @@
-package com.codershil.newshunt.keys;
-
-public class Keys {
-    private static final String weatherKey = "debc049655f451f4f21a64ff3c0a6eee";
-
-    public static String getWeatherKey() {
-        return weatherKey;
-    }
-}
